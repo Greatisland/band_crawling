e2de9c5b37e45fc1308d35b238a5a7acd4423655
THISISHYEONJIN <dontsendloveletters@gmail.com>
2024-02-08 14:59:02 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,8 +14,8 @@ import datetime
 # Selenium 설정
 chrome_options = Options()
 options = [
-    # "--headless",
-    # "--disable-gpu",
+    "--headless",
+    "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
     "--disable-extensions",
