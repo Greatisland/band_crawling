754fd2bfbdaceb055b1f44bd2cb4a3a9d74e97a6
THISISHYEONJIN <dontsendloveletters@gmail.com>
2024-02-08 13:37:26 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+# from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -40,7 +40,22 @@ import pyperclip
 
 
 # Selenium 설정
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# chrome_options = Options()
+# options = [
+#     "--headless",
+#     "--disable-gpu",
+#     "--window-size=1920,1200",
+#     "--ignore-certificate-errors",
+#     "--disable-extensions",
+#     "--no-sandbox",
+#     "--disable-dev-shm-usage"
+# ]
+# for option in options:
+#     chrome_options.add_argument(option)
+# driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+# Selenium 설정
 chrome_options = Options()
 options = [
     "--headless",
@@ -51,8 +66,11 @@ options = [
     "--no-sandbox",
     "--disable-dev-shm-usage"
 ]
+
 for option in options:
     chrome_options.add_argument(option)
+
+chrome_service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 def band_scrapping():
